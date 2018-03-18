b50a666f9d6172f203790d70846bebc8eb8a7b8f
YZ <ing.zhang@gmail.com>
2018-03-18 16:16:09 -0700

    Improve the JS Codes
    1.  Make sure the DOM is loaded before querying the DOM elements;
    2. Cache the variables on the top;
    3. Fix the bug of width and height;
    4. Re-write the events of clear buttons
        a. add a test to confirm the event is triggered;
        b. empty canvas' children when the button is clicked;
        c. set the mdl textfields to their initial states;
        d. add preventDefault().

--- designs.js
+++ designs.js
@@ -1,29 +1,52 @@
+/* Make sure that the DOM is loaded before querying the DOM elements. Refer $( document ).ready() and $(function)*/
+$(function(){
 
-$('#btn_generate').on('click',function(evt){
-	$('#pixelCanvas').html('');
-	var rows = $('input[name="height"]').val(),
+	/*Cache the variables on the top. 
+	This will prevent querying the DOM again and again, which helps in performance.*/
+ 	var rows;
+  	var cols;
+
+    
+    /* -- trigger generate button -- */
+    $('#btn_generate').on('click',function(evt){
+		$('#pixelCanvas').html('');
+	    rows = $('input[name="height"]').val(),
 	    cols = $('input[name="width"]').val();
 
-	for (var i = 0; i < rows; i++){
-		$('#pixelCanvas').append('<tr></tr>')
-	}
+		for (var i = 0; i < rows; i++){
+			$('#pixelCanvas').append('<tr></tr>');
+		};
 
-	for (var t = 0; t < cols; t++){
-		$('#pixelCanvas tr').each(function(){
-			$(this).append('<td></td>')
-		})
-	}
+	    for (var t = 0; t < cols; t++){
+		   $('#pixelCanvas tr').each(function(){
+			  $(this).append('<td></td>');
+		   });
+	    };
+	    // Prevent the default behavior of submitting 
+	     evt.preventDefault(); 
+    });
 
-	// Prevent the default behavior of submitting 
-	evt.preventDefault(); 
+
+    /* -- trigger clear button -- */
+    $('#btn_clear').on('click',function(evt){
+       console.log("dwedwd");
+       // remove all the children tags,<tr><td>s, from the table
+	   $('#pixelCanvas').empty();
+   	   // set the mdl textfield to its initial state
+       $('.is-dirty').removeClass('is-dirty');
+        // clear the textfield values
+       $('.mdl-textfield__input').val(null);
+       evt.preventDefault();	  
+     });
+
+     /* -- click each grid on the canvas -- */
+    $('#pixelCanvas').click(function(evt) {
+	   let color = $('#colorPicker').val();
+	   $(evt.target).css('background-color', color);
+    });
+
 });
 
-$('#btn_clear').on('click',function(evt){
-	$('table').remove();
-});
 
-$('#pixelCanvas').click(function(evt) {
-		let color = $('#colorPicker').val();
-		$(evt.target).css('background-color', color);
-});
+
 
